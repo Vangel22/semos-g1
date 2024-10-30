b4c31e15e522438c315b6bd2bedb60f1792276fd
Vangel22 <h.vangel22@gmail.com>
2024-10-30 20:10:54 +0100

    Save new routes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const app = express();
 
+// data doagja od nasata MongoDB baza
+const data = [
+  {
+    id: 1,
+    name: "Ace",
+  },
+  {
+    id: 2,
+    name: "Riste",
+  },
+  {
+    id: 3,
+    name: "Ceca",
+  },
+];
+
 app.get("/", (req, res) => {
   return res.send("Hello World");
+});
+
+app.get("/test", (req, res) => {
+  // /test?name="Test"
+  return res.send(
+    `Hello ${req.query.name !== "" ? req.query.name : "John Doe"}`
+  );
+});
+
+app.get("/data", (req, res) => {
+  return res.send(data);
 });
 
 app.listen(10000, () => {
